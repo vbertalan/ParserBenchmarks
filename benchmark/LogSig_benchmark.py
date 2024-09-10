f9c3682ee7df7b40638b262f7b9c62f428e84214
vbertalan <vbertalan@gmail.com>
2024-09-10 18:41:32 -0300

    TO BE FIXED

--- benchmark/LogSig_benchmark.py
+++ benchmark/LogSig_benchmark.py
@@ -13,68 +13,68 @@ input_dir = "logs"
 output_dir = "results/LogSig_result/"  # The output directory of parsing results
 
 benchmark_settings = {
-    'HDFS': {
-        'log_file': 'HDFS/HDFS_2k.log',
-        'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
-        'regex': [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'],
-        'groupNum': 15
-        },
+    # 'HDFS': {
+    #     'log_file': 'HDFS/HDFS_2k.log',
+    #     'log_format': '<Date> <Time> <Pid> <Level> <Component>: <Content>',
+    #     'regex': [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'],
+    #     'groupNum': 15
+    #     },
 
-    'Hadoop': {
-        'log_file': 'Hadoop/Hadoop_2k.log',
-        'log_format': '<Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
-        'regex': [r'(\d+\.){3}\d+'],
-        'groupNum': 30
-        },
+    # 'Hadoop': {
+    #     'log_file': 'Hadoop/Hadoop_2k.log',
+    #     'log_format': '<Date> <Time> <Level> \[<Process>\] <Component>: <Content>',
+    #     'regex': [r'(\d+\.){3}\d+'],
+    #     'groupNum': 30
+    #     },
 
-    'Spark': {
-        'log_file': 'Spark/Spark_2k.log',
-        'log_format': '<Date> <Time> <Level> <Component>: <Content>',
-        'regex': [r'(\d+\.){3}\d+', r'\b[KGTM]?B\b', r'([\w-]+\.){2,}[\w-]+'],
-        'groupNum': 20
-        },
+    # 'Spark': {
+    #     'log_file': 'Spark/Spark_2k.log',
+    #     'log_format': '<Date> <Time> <Level> <Component>: <Content>',
+    #     'regex': [r'(\d+\.){3}\d+', r'\b[KGTM]?B\b', r'([\w-]+\.){2,}[\w-]+'],
+    #     'groupNum': 20
+    #     },
 
-    'Zookeeper': {
-        'log_file': 'Zookeeper/Zookeeper_2k.log',
-        'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
-        'regex': [r'(/|)(\d+\.){3}\d+(:\d+)?'],
-        'groupNum': 46
-        },
+    # 'Zookeeper': {
+    #     'log_file': 'Zookeeper/Zookeeper_2k.log',
+    #     'log_format': '<Date> <Time> - <Level>  \[<Node>:<Component>@<Id>\] - <Content>',
+    #     'regex': [r'(/|)(\d+\.){3}\d+(:\d+)?'],
+    #     'groupNum': 46
+    #     },
 
-    'BGL': {
-        'log_file': 'BGL/BGL_2k.log',
-        'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
-        'regex': [r'core\.\d+'],
-        'groupNum': 500
-        },
+    # 'BGL': {
+    #     'log_file': 'BGL/BGL_2k.log',
+    #     'log_format': '<Label> <Timestamp> <Date> <Node> <Time> <NodeRepeat> <Type> <Component> <Level> <Content>',
+    #     'regex': [r'core\.\d+'],
+    #     'groupNum': 500
+    #     },
 
-    'HPC': {
-        'log_file': 'HPC/HPC_2k.log',
-        'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
-        'regex': [r'=\d+'],
-        'groupNum': 800
-        },
+    # 'HPC': {
+    #     'log_file': 'HPC/HPC_2k.log',
+    #     'log_format': '<LogId> <Node> <Component> <State> <Time> <Flag> <Content>',
+    #     'regex': [r'=\d+'],
+    #     'groupNum': 800
+    #     },
 
-    'Thunderbird': {
-        'log_file': 'Thunderbird/Thunderbird_2k.log',
-        'log_format': '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>',
-        'regex': [r'(\d+\.){3}\d+'],
-        'groupNum': 25
-        },
+    # 'Thunderbird': {
+    #     'log_file': 'Thunderbird/Thunderbird_2k.log',
+    #     'log_format': '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>',
+    #     'regex': [r'(\d+\.){3}\d+'],
+    #     'groupNum': 25
+    #     },
 
-    'Windows': {
-        'log_file': 'Windows/Windows_2k.log',
-        'log_format': '<Date> <Time>, <Level>                  <Component>    <Content>',
-        'regex': [r'0x.*?\s'],
-        'groupNum': 42
-        },
+    # 'Windows': {
+    #     'log_file': 'Windows/Windows_2k.log',
+    #     'log_format': '<Date> <Time>, <Level>                  <Component>    <Content>',
+    #     'regex': [r'0x.*?\s'],
+    #     'groupNum': 42
+    #     },
 
-    'Linux': {
-        'log_file': 'Linux/Linux_2k.log',
-        'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
-        'regex': [r'(\d+\.){3}\d+', r'\d{2}:\d{2}:\d{2}'],
-        'groupNum': 25
-        },
+    # 'Linux': {
+    #     'log_file': 'Linux/Linux_2k.log',
+    #     'log_format': '<Month> <Date> <Time> <Level> <Component>(\[<PID>\])?: <Content>',
+    #     'regex': [r'(\d+\.){3}\d+', r'\d{2}:\d{2}:\d{2}'],
+    #     'groupNum': 25
+    #     },
 
     'Andriod': {
         'log_file': 'Andriod/Andriod_2k.log',
