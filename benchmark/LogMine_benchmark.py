05fc4211345b08076c98da7044790cf5ae690acf
vbertalan <vbertalan@gmail.com>
2024-09-10 18:22:51 -0300

    correcting codes

--- benchmark/LogMine_benchmark.py
+++ benchmark/LogMine_benchmark.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append("C:/Users/vbert/OneDrive/DOUTORADO Poly Mtl/Projeto/parser-1")
-from logparser.logparser.utils import evaluator
-from logparser.logparser.LogMine import LogMine
-import os
-import pandas as pd
-from pathlib import Path
+sys.path.append('../ParserBenchmarks')
 
-input_dir = "logparser/logs"
-output_dir = "logparser/results/LogMine_result/"  # The output directory of parsing results
+from logparser.utils import evaluator
+from logparser.LogMine import LogMine
+from pathlib import Path
+import pandas as pd
+import os
+
+input_dir = "logs"
+output_dir = "results/LogMine_result/"  # The output directory of parsing results
 
 benchmark_settings = {
     'HDFS': {
@@ -179,5 +180,5 @@ print('\n=== Overall evaluation results ===')
 df_result = pd.DataFrame(bechmark_result, columns=['Dataset', 'Precision', 'Recall', 'F1 Measure', 'Accuracy'])
 df_result.set_index('Dataset', inplace=True)
 print(df_result)
-filepath = Path('logparser/results/LogMine_bechmark_result.csv') 
+filepath = Path('results/LogMine_bechmark_result.csv') 
 df_result.T.to_csv(filepath)
